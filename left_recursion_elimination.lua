b92a4b86b0ca23dd817c96f5c73be05afae1accb
Lee Gao <leegao@fb.com>
2015-11-04 17:58:08 -0800

    finished nullability elimination

--- left_recursion_elimination.lua
+++ left_recursion_elimination.lua
@@ -6,7 +6,44 @@ local ll1 = require 'll1'
 local utils = require 'utils'
 
 local function hash(production)
-  return utils.dump {unpack(production)}
+  return table.concat({unpack(production)}, '^^^')
+end
+
+local function normalize(production)
+  local new = {}
+  for object in utils.loop(production) do
+    if object ~= '' then
+      table.insert(new, object)
+    end
+  end
+  return new
+end
+
+local function null_out(production, i, nullable_indices)
+  local new = {unpack(production)}
+  local changed = {}
+  local index = 1
+  while true do
+    local okay = i % 2 == 0
+    
+    if okay then
+      new[nullable_indices[index]] = ''
+      table.insert(changed, nullable_indices[index])
+    end
+    
+    i = math.floor(i / 2)
+    if i == 0 then break end
+    index = index + 1
+  end
+  return normalize(new)
+end
+
+local function insert_into(new_nonterminal, production, production_hashes)
+  local h = hash(production)
+  if not production_hashes[h] and #production ~= 0 then
+    table.insert(new_nonterminal, production)
+    production_hashes[h] = true
+  end
 end
 
 local function eliminate_nullables(configuration)
@@ -18,6 +55,7 @@ local function eliminate_nullables(configuration)
     end
   end
   
+  local new_actions = {}
   for variable, nonterminal in pairs(configuration) do
     -- let's construct a hashset of the original productions
     local seen_productions = {}
@@ -26,6 +64,8 @@ local function eliminate_nullables(configuration)
     end
     
     -- let's compute the null-eliminated expansion
+    local new_nonterminal = {}
+    local production_hashes = {}
     for production in utils.loop(nonterminal) do
       local action = production.action
       local nullable_indices = {}
@@ -34,39 +74,45 @@ local function eliminate_nullables(configuration)
           table.insert(nullable_indices, i)
         end
       end
-      -- compute the combinatorial transfer to naturals
-      print(variable, table.concat(production, ' '))
-      for i=0,2^#nullable_indices - 2 do
-        print(i)
+      if #nullable_indices ~= 0 then
+        -- compute the combinatorial transfer to naturals
+        for i=0,2^#nullable_indices - 1 do
+          local new_production, changed = null_out(production, i, nullable_indices)
+          insert_into(new_nonterminal, new_production, production_hashes)
+        end
+      else
+        insert_into(new_nonterminal, normalize(production), production_hashes)
       end
     end
+    new_actions[variable] = new_nonterminal
+    assert(#new_nonterminal ~= 0)
   end
+  return ll1.configure(new_actions)
 end
 
 -- testing
 local configuration = ll1.configure {
   root = {
-    {'$expr', action = id},
+    {'$S'},
   },
-  rexpr = {
-    {'', action = id},
-    {'$expr', action = id},
-    {'+', '$expr', action = id},
+  S = {
+    {'$X', '$X'},
+    {'$Y'},
   },
-  expr = {
-    {'$consts', '$rexpr', action = id},
-    {'identifier', '$rexpr', action = id},
-    {'fun', 'identifier', '->', '$expr', action = id},
-    {'(', '$expr', ')', '$rexpr', action = id},
+  X = {
+    {'a', '$X', 'b'},
+    {''},
   },
-  consts = {
-    {'number', action = id},
-    {'string', action = id},
-    {'true', action = id},
-    {'false', action = id},
+  Y = {
+    {'a', '$Y', 'b'},
+    {'$Z'},
+  },
+  Z = {
+    {'b', '$Z', 'a'},
+    {''},
   }
 }
 
-eliminate_nullables(configuration)
+local new_configuration = eliminate_nullables(configuration)
 
 return left_recursion_elimination
