228a4c0cedeab58dc6612b6329cd1f0878150edd
Lee Gao <leegao@fb.com>
2015-11-14 22:21:06 -0800

    added ast visitor for the concrete lua ast

--- hello.lua
+++ hello.lua
@@ -6,7 +6,7 @@ local re = require 'parsing.re'
 --print(config:pretty())
 --print(utils.to_list(config:follow('block')))
 --print(config:follows():dot())
-ll1.yacc(parser.grammar)
+--ll1.yacc(parser.grammar)
 -- print(utils.to_list(config:get_dependency_graph().reverse['suffixedexp\'star#1']))
 --print(utils.to_list(config:first('stat')))
 
@@ -57,4 +57,126 @@ ll1.yacc(parser.grammar)
 --local tree = parser 'function graph.vertices(self) end'
 
 local tree = parser(io.open('ll1/ll1.lua', 'r'):read('*all'))
-print(tree)
+local ast = require 'lua.ast'
+
+local visitor = ast {
+  before = function(self, node)
+    local keys = {}
+    if self['on_' .. node.kind] then
+      return true
+    end
+    for key in pairs(node) do 
+      if type(key) == 'string' and key ~= 'kind' and key ~= 'parent' then 
+        table.insert(keys, key) 
+      end 
+    end
+    table.sort(keys)
+    print(node.kind, unpack(keys))
+  end,
+  on_block = function(self, node)
+    -- ret
+    return true
+  end,
+  on_localassign = function(self, node)
+    -- left, right
+    return true
+  end,
+  on_names = function(self, node)
+    -- list of leaves of Names
+    return true
+  end,
+  on_leaf = function(self, node)
+    -- tokens
+    return true
+  end,
+  on_explist = function(self, node)
+    -- list of node 'exp's
+    return true
+  end,
+  on_table = function(self, node)
+    -- list of table node 'elements'
+    return true
+  end,
+  on_call = function(self, node)
+    -- target, args
+    return true
+  end,
+  on_args = function(self, node)
+    -- list of node 'exp's
+    return true
+  end,
+  on_unop = function(self, node)
+    -- operator, operand
+    return true
+  end,
+  on_element = function(self, node)
+    -- index, value
+    return true
+  end,
+  on_function = function(self, node)
+    -- parameters, body
+    return true
+  end,
+  on_parameters = function(self, node)
+    -- list of names, vararg
+    return true
+  end,
+  on_return = function(self, node)
+    -- explist
+    return true
+  end,
+  on_selfcall = function(self, node)
+    -- target, args
+    return true
+  end,
+  on_index = function(self, node)
+    -- left, right
+    return true
+  end,
+  on_if = function(self, node)
+    -- cond, block, elseifs, else
+    return true
+  end,
+  on_foreach = function(self, node)
+    -- names, iterator, block
+    return true
+  end,
+  on_binop = function(self, node)
+    -- left, right
+    return true
+  end,
+  on_assignments = function(self, node)
+    -- left, right
+    return true
+  end,
+  on_lvalue = function(self, node)
+    -- list of lvals (primaryexp suffix*)
+    return true
+  end,
+  on_else = function(self, node)
+    -- block
+    return true
+  end,
+  on_localfunctiondef = function(self, node)
+    -- name, function
+    return true
+  end,
+  on_functiondef = function(self, node)
+    -- funcname, function
+    return true
+  end,
+  on_funcnames = function(self, node)
+    -- list of funcname*, colon
+    return true
+  end,
+  on_empty = function(self, node)
+    -- nothing
+    return true
+  end,
+  on_elseif = function(self, node)
+    -- block cond
+    return true
+  end,
+}
+
+visitor:accept(tree)
--- lua/ast.lua
+++ lua/ast.lua
@@ -2,17 +2,36 @@ local utils = require 'common.utils'
 
 local ast = {}
 
+function ast:before(node)
+  
+end
+
+function ast:after(node)
+  
+end
+
 function ast:accept(node)
+  if not node.kind then
+    return
+  end
+  self:before(node)
   local action = self['on_' .. node.kind]
   local continue = true
   if action then
     continue = action(self, node)
   end
   if continue then
-    for child in utils.loop(self) do
+    for child in utils.loop(node) do
       self:accept(child)
     end
   end
+  self:after(node)
 end
 
-return ast
+return setmetatable(
+  ast, 
+  {
+    __call = function(self, visitor) 
+      return setmetatable(visitor, {__index = ast}) 
+    end
+  })
