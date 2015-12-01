58ef5f82a86e7dc96eb9e314ac9c6818b5c34e67
Lee Gao <leegao@fb.com>
2015-11-30 20:32:05 -0600

    dump function and code and constants

--- bytecode/dump.lua
+++ bytecode/dump.lua
@@ -4,6 +4,7 @@ local undump = require "bytecode.undump" -- needed for sizet info
 local opcode = require "bytecode.opcode"
 local writer = require "bytecode.writer"
 local ir = require 'bytecode.ir'
+local utils = require 'common.utils'
 local parser = require 'lua.parser'
 local compiler = require 'lua.interpreter'
 
@@ -12,10 +13,58 @@ local compiler = require 'lua.interpreter'
 --undump.sizeof_instruction = ?
 --undump.sizeof_number = 8
 
+local function generic_list(ctx, list, serializer, size)
+  local n = #list
+  local out = ctx.writer
+  out:int(n, size)
+  for object in utils.loop(list) do
+    serializer(ctx, object)
+  end
+end
+
+function dump.dump_code(ctx, code)
+  local out = ctx.writer
+  generic_list(
+    ctx,
+    code,
+    function(ctx, instruction)
+      out:int(opcode.serialize(instruction), undump.sizeof_instruction)
+    end)
+end
+
+function dump.dump_constants(ctx, constants)
+  local out = ctx.writer
+  generic_list(
+    ctx,
+    constants,
+    function(_, object)
+      local t = type(object)
+      if t == 'number' then
+        out:byte(3)
+        out:double(object)
+      elseif t == 'boolean' then
+        out:byte(1)
+        out:byte(object and 1 or 0)
+      elseif t == 'string' then
+        out:byte(4)
+        out:string(object)
+      else
+        out:byte(0)
+      end
+    end)
+  generic_list(ctx, constants.functions, dump.dump_function)
+end
+
 function dump.dump_function(ctx, closure)
   local out = ctx.writer
   out:int(closure.first_line)
   out:int(closure.last_line)
+  out:byte(closure.nparams)
+  out:byte(closure.is_vararg and 1 or 0)
+  dump.dump_code(ctx, closure.code)
+  dump.dump_constants(ctx, closure.constants)
+--  dump.dump_upvalues(ctx, closure.upvalues)
+--  dump.dump_debug(ctx, closure.debug)
 end
 
 local ctx = {writer = writer.new_writer()}
